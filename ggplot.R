583777d173f31c41f8e53be4d8410399664d93dc
Naupaka Zimmerman <naupaka@gmail.com>
2015-01-11 14:46:46 -0700

    Added some ggplot code

--- ggplot.R
+++ ggplot.R
@@ -4,3 +4,28 @@ install.packages("ggthemes")
 install.packages("reshape2")
 
 library("ggplot2")
+
+head(iris)
+
+myplot <- ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width))
+summary(myplot)
+myplot + geom_point()
+
+ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width)) + geom_point()
+
+library("reshape2")
+df <- melt(iris, id.vars = "Species")
+ggplot(df, aes(Species, value, fill = variable)) +
+    geom_bar(stat = "identity", position = "dodge") +
+    scale_fill_brewer(palette = "Set1")
+￼
+h <- ggplot(faithful, aes(x = waiting))
+h + geom_histogram(binwidth = 20, colour = "black")
+
+our.faithful <- faithful
+
+
+
+
+
+
